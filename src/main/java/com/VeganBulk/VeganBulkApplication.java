2565ecad98cf2b8850557a8e9a01ef34e45b09fd
LAPTOP-ONT9E2DS\raque <raquel.valdiviesogalan@gmail.com>
2026-05-29 11:06:12 +0200

    init

--- src/main/java/com/VeganBulk/VeganBulkApplication.java
+++ src/main/java/com/VeganBulk/VeganBulkApplication.java
@@ -1,13 +1,13 @@
-package com.VeganBulk.demo;
+package com.veganbulk;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class VeganBulkApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(VeganBulkApplication.class, args);
 	}
 
 }
